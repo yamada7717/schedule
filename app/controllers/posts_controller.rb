40cc6fa255f60d7ef949e576dd062940e165c594
yamada7717 <hiro.program17@gmail.com>
2023-05-22 04:34:08 +0900

    views/posts/ページの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,41 @@
 class PostsController < ApplicationController
-  def def index
+  def index
+    @posts = Post.all
+  end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(
+      :content,
+      :title,
+      :start_date,
+      :end_date,
+      :introduction
+      ))
+    if @post.save
+        flash[:notice] = "スケジュールの予約が完了しました"
+        redirect_to :posts
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "投稿を消去しました"
+    redirect_to :posts
   end
 end
